846058607f31391f3ba7bea9d7bb1bf306025a41
shahwuang <shahuwang@qq.com>
2013-12-12 20:29:24 +0800

    update

--- rest/shop_struct.go
+++ rest/shop_struct.go
@@ -1,25 +1,31 @@
 package rest
 
+import "time"
+
 type ShopScore struct {
-	DeliveryScore float32 `json:"delivery_score,string" bson:"delivery_score"`
-	ItemScore     float32 `json:"item_score,string" bson:"item_score"`
-	ServiceScore  float32 `json:"service_score,string" bson:"service_score"`
+	DeliveryScore float32   `json:"delivery_score,string" bson:"delivery_score"`
+	ItemScore     float32   `json:"item_score,string" bson:"item_score"`
+	ServiceScore  float32   `json:"service_score,string" bson:"service_score"`
+	Credit        string    `json:"credit" bson:"credit"`
+	PraiseRate    float32   `json:"praise_rate" bson:"praise_rate"`
+	UpdatedTime   time.Time `json:"updated_time" bson:"updated_time"`
 }
 
 type Shop struct {
-	AllCount    int        `json:"all_count" bson:"all_count"`
-	Bulletin    string     `json:"bulletin" bson:"bulletin,omitempty"`
-	Cid         int        `json:"cid" bson:"cid"`
-	Created     string     `json:"created" bson:"created"`
-	Desc        string     `json:"desc" bson:"desc"`
-	Modified    string     `json:"modified" bson:"modified"`
-	Nick        string     `json:"nick" bson:"nick"`
-	PicPath     string     `json:"pic_path" bson:"pic_path"`
-	RemainCount int        `json:"remain_count" bson:"remain_count"`
-	ShopScore   *ShopScore `json:"shop_score" bson:"shop_score"`
-	Sid         int        `json:"sid" bson:"sid"`
-	Title       string     `json:"title" bson:"title"`
-	UsedCount   int        `json:"used_count" bson:"used_count"`
+	AllCount     int        `json:"all_count" bson:"all_count"`
+	Bulletin     string     `json:"bulletin" bson:"bulletin,omitempty"`
+	Cid          int        `json:"cid" bson:"cid"`
+	Created      string     `json:"created" bson:"created"`
+	Desc         string     `json:"desc" bson:"desc"`
+	Modified     string     `json:"modified" bson:"modified"`
+	Nick         string     `json:"nick" bson:"nick"`
+	PicPath      string     `json:"pic_path" bson:"pic_path"`
+	RemainCount  int        `json:"remain_count" bson:"remain_count"`
+	ShopScore    *ShopScore `json:"shop_score" bson:"shop_score"`
+	Sid          int        `json:"sid" bson:"sid"`
+	Title        string     `json:"title" bson:"title"`
+	UsedCount    int        `json:"used_count" bson:"used_count"`
+	MainProducts string     `json:"main_products" bson:"main_products"`
 }
 
 type ShopGetResponse struct {
